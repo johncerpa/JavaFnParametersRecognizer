3301d7d2cdfd3a11e2cdba817a303610969ce1fe
Jhon Cerpa <jjcerpa@uninorte.edu.co>
2020-05-24 00:53:59 -0500

    Finished

--- Parametros2.cs
+++ Parametros2.cs
@@ -24,6 +24,89 @@ namespace Parametros2
                 string[] elems = System.Text.RegularExpressions.Regex.Split(ParamValid, pattern);
 
                 bool noHayError = true;
+                bool hayIdsRepetidos = false;
+                int elemsAVerificar = 0;
+
+                String[] id = new String[elems.Length]; // Vector de identificadores
+
+                // Guardar identificadores
+                for (int i = 0; i < elems.Length; i++)
+                {
+                    int corchAbre = elems[i].IndexOf("["); // indice del primer corchete abre
+                    int corchCierr = elems[i].IndexOf("]"); // indice del primer corchete cierra
+                    if (corchAbre == -1 && corchCierr == -1)
+                    {
+                        // Se busca el primer indice de un espacio
+                        int spaceIdx = elems[i].IndexOf(" ");
+
+                        if (spaceIdx > -1)
+                        {
+                            // Expresion regular para separar pos un espacio o más
+                            // Ejemplo: "Hola a" => ["Hola", "a"], ["int", "b"]
+                            string pt = @"\s+";
+                            string[] valor = System.Text.RegularExpressions.Regex.Split(elems[i], pt);
+
+                            if (valor.Length == 2)
+                            {
+                                // Si se encuentran el tipo y el identificador sin error, se guardan
+                                id[elemsAVerificar] = valor[1]; // Identificadores para comparar que no se repitan
+                                ++elemsAVerificar;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        // Entra aqui si el parametro es un vector o una matriz
+                        // Ej.: int[] a, int[][]b, int c[][]
+
+                        // Busca el ultimo indice de "]"
+                        int corchUltimoIdx = elems[i].LastIndexOf("]");
+
+                        // Si no se encuentra el indice de "]" se marca el error
+                        if (corchUltimoIdx > -1)
+                        {
+                            // En caso de no encontrar error, se guardan
+                            if (corchUltimoIdx == elems[i].Length - 1)
+                            {
+                                // Ejemplo: Clase a[][], int b [] []
+                                int primerCorch = elems[i].IndexOf("[");
+
+                                if (primerCorch > -1)
+                                {
+                                    string toSplit = elems[i].Substring(0, primerCorch).Trim();
+
+                                    string patron = @"\s+";
+                                    string[] valor = System.Text.RegularExpressions.Regex.Split(toSplit, patron);
+
+                                    if (valor.Length == 2)
+                                    {
+                                        id[elemsAVerificar] = valor[1];
+                                        ++elemsAVerificar;
+                                    }
+                                }
+                            }
+                            else if (corchUltimoIdx < elems[i].Length - 1)
+                            {
+                                id[elemsAVerificar] = elems[i].Substring(corchUltimoIdx + 1);
+                                ++elemsAVerificar;
+                            }
+                        }
+                    }
+
+                    // Verificar que identificadores no sean iguales
+                    for (int y = 0; y < elemsAVerificar - 1; y++)
+                    {
+                        for (int j = y + 1; j < elems.Length; j++)
+                        {
+                            if (id[y] == id[j])
+                            {
+                                Console.WriteLine("Error, hay identificadores repetidos");
+                                hayIdsRepetidos = true;
+                            }
+                        }
+                    }
+                }
+
                 for (int i = 0; i < elems.Length; i++)
                 {
                     String substr = elems[i].Substring(0, 5);
@@ -39,7 +122,7 @@ namespace Parametros2
                     }
                 }
 
-                if (noHayError)
+                if (noHayError && !hayIdsRepetidos)
                 {
                     Console.WriteLine("Los parametros estan bien escritos");
                 }
